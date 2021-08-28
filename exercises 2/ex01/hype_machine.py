4d3dc11721d4fd689590050407b32610bd7e420b
Soul Dayan <souljade@live.unc.edu>
2021-08-28 18:21:01 -0400

    push to:

---
+++ exercises 2/ex01/hype_machine.py
@@ -0,0 +1,8 @@
+"""Hype machine to boost my spirits and gas me up!"""
+
+__author__ = "730327594"
+
+name: str = input("What is your name? ")
+print("What's up " + name + " you're a beast.")
+print(name + " you are actually coding right now dude!") 
+print("This is the first of many awesome programs! Keep up the amazing work " + name + "!")
--- exercises 2/ex01/relational_operators.py
+++ exercises 2/ex01/relational_operators.py
@@ -5,7 +5,7 @@ __author__ = "730327594"
 first_num: str = input("enter the first number: ")
 second_num: str = input("enter the second number: ")
 
-print(first_num + " > " + second_num + " is " + str(first_num < second_num))
+print(first_num + " < " + second_num + " is " + str(first_num < second_num))
 print(first_num + " >= " + second_num + " is " + str(first_num >= second_num))
 print(first_num + " == " + second_num + " is " + str(first_num == second_num))
 print(first_num + " != " + second_num + " is " + str(first_num != second_num))
