b0416f2960133f4475bd3064e7f013cf9f669988
Soul Dayan <souljade@live.unc.edu>
2021-08-28 18:28:46 -0400

    ex01 final

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,8 @@
-# TODO: Write docstring here
+"""Hype machine to boost my spirits and gas me up!"""
 
-# TODO: Initialize __author__ variable here
+__author__ = "730327594"
 
-# TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("What's up " + name + " you're a beast.")
+print(name + " you are actually coding right now dude!") 
+print("This is the first of many awesome programs! Keep up the amazing work " + name + "!")
