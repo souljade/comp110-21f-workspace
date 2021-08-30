6563a712654fff162d8be40e6f8b7557b8ee48b4
Soul Dayan <souljade@live.unc.edu>
2021-08-30 15:01:03 -0400

    removed whitespace

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -2,10 +2,10 @@
 
 __author__ = "730327594"
 
-first_num: int = int( input("Enter the first number: "))
+first_num: int = int(input("Enter the first number: "))
 second_num: int = int(input("Enter the second number: "))
 
-print(str (first_num) + " < " + str(second_num) + " is " + str(first_num < second_num))
+print(str(first_num) + " < " + str(second_num) + " is " + str(first_num < second_num))
 print(str(first_num) + " >= " + str(second_num) + " is " + str(first_num >= second_num))
 print(str(first_num) + " == " + str(second_num) + " is " + str(first_num == second_num))
 print(str(first_num) + " != " + str(second_num) + " is " + str(first_num != second_num))
